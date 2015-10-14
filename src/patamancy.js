9524771b929d4df6827d5478d33595522f2257e2
Henri Bourcereau <henri.bourcereau@gmail.com>
2015-10-14 12:31:04 +0200

    fix quote extraction

--- src/patamancy.js
+++ src/patamancy.js
@@ -19,15 +19,15 @@ export function randomQuoteId(){
 
 function quoteAt(size, book, position){
   let newpos = position - book.content.length;
+  const nbChapters = book.books.length; 
 
-  if (newpos + minSize <= 0 ) {
+  if (newpos + minSize <= 0 || nbChapters == 0) {
     return {
       quote: getQuote(size, book.content, position),
       chapter: [book.title]
     };
   }
 
-  const nbChapters = book.books.length; 
   let numChapter = 0;
   let redChapter = book;
   while (newpos + minSize > 0 && numChapter < nbChapters) {
@@ -50,38 +50,66 @@ function getQuote(size, text, position){
   const chunkStop = Math.min(text.length, chunkStart + maxTextChunk);
   const chunk = text.slice(chunkStart, chunkStop);
 
+  console.log(chunk);
+
   //Try to get complete sentences of the chunk of text 
   let sentences = tokenizer.sentences(chunk, true);
-  let smallChunkLength = chunk.length - sentences[0].length;
   let removedFromStart = 0;
+  let smallChunkLength = chunk.length - sentences[0].length;
   if (sentences.length > 1 && (smallChunkLength > minSize/2)) {
     //Remove first sentence (probably incomplete)
     removedFromStart = sentences[0].length;
     sentences = sentences.slice(1);
-    smallChunkLength = smallChunkLength - sentences.slice(-1)[0].length;
+    smallChunkLength = smallChunkLength - sentences[sentences.length-1].length;
     
-    if (sentences.length > 1 && (smallChunkLength > minSize/2)) {
+    if (sentences.length > 1 && (smallChunkLength > minSize/2) && (chunkStop < text.length)) {
       //Remove last sentence, in order to end on a complete sentence
       sentences = sentences.slice(0,-1);
     }
   }
 
   //Get the minimum number of sentences needed to attain the given size
-  let quoteLength = 0;
-  let numSentence = 0;
+  //We start with the sentence containing the position
+  let beforeLength = position - chunkStart;
+  let beforeSentences = [];
+  let afterSentences = [];
+  let quoteSentences = []; 
   for (let sentence of sentences){
-    quoteLength += sentence.length;
-    numSentence += 1;
-    if (quoteLength > minSize) break;
+    beforeLength += sentence.length;
+    if (beforeLength < position) {
+      beforeSentences.unshift(sentence);
+    } else if (quoteSentences.length == 0) {
+      quoteSentences.push(sentence);
+    } else {
+      afterSentences.push(sentence);
+    }
   }
 
-  const quoteStart = chunk.indexOf(sentences[0]);
-  const lastSentence = sentences[numSentence - 1]
-  const quoteStop = removedFromStart + chunk.slice(removedFromStart).indexOf(lastSentence) + lastSentence.length;
+  // console.log(beforeSentences);
+  // console.log(quoteSentences);
+  // console.log(afterSentences);
 
-  // console.log(sentences);
-  // console.log(quoteStart);
-  // console.log(quoteStop);
+  let quoteLength = 0;
+  if (quoteSentences.length > 0){
+    quoteLength = quoteSentences[0].length;
+  }
+  //We add the following sentences if necessary
+  for (let sentence of afterSentences){
+    if (quoteLength > minSize) break;
+    quoteSentences.push(sentence);
+    quoteLength += sentence.length;
+  }
+
+  //We add the previous sentences if necessary
+  for (let sentence of beforeSentences){
+    if (quoteLength > minSize) break;
+    quoteSentences.unshift(sentence);
+    quoteLength += sentence.length;
+  }
+
+  const quoteStart = chunk.indexOf(quoteSentences[0]);
+  const lastSentence = quoteSentences[quoteSentences.length - 1]
+  const quoteStop = removedFromStart + chunk.slice(removedFromStart).indexOf(lastSentence) + lastSentence.length;
 
   return cleanText(chunk.slice(quoteStart, quoteStop));
 }
@@ -91,4 +119,4 @@ function cleanText(text){
   return text.trim();
 }
 
-// console.log(randomQuote({position:56863}));
+// console.log(randomQuote({position:24430}));
